8a2f9ea5a70d331dd7fb00c9965906ccd44d1dfd
Eirado <111468283+Eirado@users.noreply.github.com>
2025-04-01 23:49:07 -0300

    Time: 7 ms (89.52%), Space: 17.8 MB (86.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -10,7 +10,6 @@ class Solution:
             dic[s[i]] = (dic.get(s[i]) or 0) + 1 
  
         for i in set(t):
-            print(i)
             if i in dic.keys():
                 if t.count(i) != dic[i]:
                     return False
