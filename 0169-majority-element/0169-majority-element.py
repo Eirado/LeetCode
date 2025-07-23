b1a9c943795cd0b60ce0da39ff12af610f0335b8
Gabriel Eirado <111468283+Eirado@users.noreply.github.com>
2025-07-23 18:49:11 -0300

    Time: 9 ms (33.55%), Space: 19.4 MB (50.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        N = len(nums)
-        my_set = set(nums)
-        for i in my_set:
-            if nums.count(i) > N // 2:
-                return i
-  
         
+        hashmap = {}
+        list_part_size = len(nums) / 2
+
+        for i in nums: 
+            hashmap[i] = hashmap.get(i,0) + 1
+            if hashmap[i] > list_part_size:
+                    return i
+                
+
+                
+            
