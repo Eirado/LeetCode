8819f2ce1f56e2b362e8c14f2499fac919128c5a
Gabriel Eirado <111468283+Eirado@users.noreply.github.com>
2025-08-16 10:04:48 -0300

    Time: 44 ms (17.49%), Space: 17.7 MB (48.55%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -1,16 +1,13 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
 
-        x, y = 0, 0
         res = []
-        range_len = min(len(word1), len(word2))
-        for i in range(range_len):
-            res.append(word1[x]) 
-            res.append(word2[y])
-            x += 1
-            y += 1
+        min_len = min(len(word1), len(word2))
+        for i in range(min_len):
+            res.append(word1[i]) 
+            res.append(word2[i])
 
-        res.append(word1[x:])
-        res.append(word2[y:])
-        
+        res.append(word1[min_len:])
+        res.append(word2[min_len:])
+
         return "".join(res)
