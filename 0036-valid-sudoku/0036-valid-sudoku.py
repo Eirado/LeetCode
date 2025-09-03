454793c5dc6ffad101e9ee46af7a98eb66b6ccbe
Gabriel Eirado <111468283+Eirado@users.noreply.github.com>
2025-09-03 15:17:21 -0300

    Time: 7 ms (36.28%), Space: 18 MB (25.05%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,6 +1,7 @@
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
         rows: defaultdict[int, set[str]] = defaultdict(set)
         colls: defaultdict[int, set[str]] = defaultdict(set)
         grid: defaultdict[int, set[str]] = defaultdict(set)
@@ -9,17 +10,12 @@ class Solution:
             for j in range(len(board[i])):
                 if board[i][j] == ".":
                     continue
-
                 if (board[i][j] in rows[i] or 
                 board[i][j] in colls[j] or 
                 board[i][j] in grid[(i // 3, j // 3)]):
                     return False
-
                 rows[i].add(board[i][j])
                 colls[j].add(board[i][j])
                 grid[(i // 3, j // 3)].add(board[i][j])
-
-    
         return True
         
-        # defaultdict(<class 'set'>, {(0, 0): set(), 0: {'6', '9', '5', '8', '3'}, (0, 1): set(), 1: {'1', '9', '5', '4', '7', '8'}, (0, 2): set(), 2: {'6', '3', '2', '8'}, (1, 0): set(), (1, 1): set(), (1, 2): set(), 3: {'1', '6', '9', '4', '8', '3'}, (2, 0): set(), (2, 2): set(), 4: {'2', '9', '5', '7', '8'}, (2, 1): set()})
