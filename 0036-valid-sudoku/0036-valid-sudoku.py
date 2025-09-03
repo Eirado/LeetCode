66c2a49480ad359fb40f4187ee71aec6dcb03ee2
Gabriel Eirado <111468283+Eirado@users.noreply.github.com>
2025-09-03 15:15:12 -0300

    Time: 6 ms (41.44%), Space: 17.9 MB (25.05%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,23 +1,25 @@
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        row_size = len(board[0])
-        
-        rule01 = False
-        count_01 = 0
-
-        rule02 = [False] * row_size
-        rule03 = [False] * row_size
+        rows: defaultdict[int, set[str]] = defaultdict(set)
+        colls: defaultdict[int, set[str]] = defaultdict(set)
+        grid: defaultdict[int, set[str]] = defaultdict(set)
 
         for i in range(len(board)):
-            
-            for j in range(row_size):
-                if board[i][j] is not ".":
-                    rule01 = True
-                    rule02[j] = True
+            for j in range(len(board[i])):
+                if board[i][j] == ".":
+                    continue
+
+                if (board[i][j] in rows[i] or 
+                board[i][j] in colls[j] or 
+                board[i][j] in grid[(i // 3, j // 3)]):
+                    return False
+
+                rows[i].add(board[i][j])
+                colls[j].add(board[i][j])
+                grid[(i // 3, j // 3)].add(board[i][j])
+
+    
+        return True
         
-            if rule01: 
-                rule01 = False
-                count_01 += 1
-        
-        return count_01 == row_size and [True] * row_size == rule02
+        # defaultdict(<class 'set'>, {(0, 0): set(), 0: {'6', '9', '5', '8', '3'}, (0, 1): set(), 1: {'1', '9', '5', '4', '7', '8'}, (0, 2): set(), 2: {'6', '3', '2', '8'}, (1, 0): set(), (1, 1): set(), (1, 2): set(), 3: {'1', '6', '9', '4', '8', '3'}, (2, 0): set(), (2, 2): set(), 4: {'2', '9', '5', '7', '8'}, (2, 1): set()})
